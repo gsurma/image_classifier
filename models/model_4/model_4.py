fd94122221db5525d3cdc3d6393039c225feb077
Greg <grzegorz.surma@icloud.com>
2018-11-15 18:24:18 -0800

    final model 5

--- models/model_4/model_4.py
+++ models/model_4/model_4.py
@@ -13,8 +13,9 @@
 	model.add(MaxPooling2D(pool_size=(2, 2)))
 
 	model.add(Flatten())
-	model.add(Dense(64))
+	model.add(Dense(128))
 	model.add(Activation('relu'))
+	model.add(Dropout(0.5))
 	model.add(Dense(1))
 	model.add(Activation('sigmoid'))
 
---
+++ models/model_5/model_5.py
@@ -0,0 +1,31 @@
+model = Sequential()
+
+model.add(Conv2D(32, 3, 3, border_mode='same', input_shape=input_shape, activation='relu'))
+model.add(Conv2D(32, 3, 3, border_mode='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, 3, 3, border_mode='same', activation='relu'))
+model.add(Conv2D(64, 3, 3, border_mode='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(128, 3, 3, border_mode='same', activation='relu'))
+model.add(Conv2D(128, 3, 3, border_mode='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(256, 3, 3, border_mode='same', activation='relu'))
+model.add(Conv2D(256, 3, 3, border_mode='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+    
+model.compile(loss='binary_crossentropy',
+            optimizer=RMSprop(lr=0.0001),
+            metrics=['accuracy'])
